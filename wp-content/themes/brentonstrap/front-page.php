17f1cdabdaafc99916f8d5d9264b7aa999c2c9eb
vrxm <nopales@Victor-M.lan>
2015-10-17 09:20:25 -0700

    cleaning up front page

--- wp-content/themes/brentonstrap/front-page.php
+++ wp-content/themes/brentonstrap/front-page.php
@@ -16,14 +16,7 @@ get_header(); ?>
 
 			<?php endwhile; // End of the loop. ?>
         
-<?php
-if( function_exists('fa_display_slider') ){
-    fa_display_slider( 87 );
-}
-?>
 
-      <div class="row">
-        <div class="row">
           <!--Query the database and show 1 post from the featured category-->
           <?php $custom_query = new
           WP_Query('posts_per_page=1&category_name=featured');
@@ -46,16 +39,16 @@ if( function_exists('fa_display_slider') ){
           </div>
           <?php endwhile; ?>
           <?php wp_reset_postdata(); // reset the query ?>
-        </div>
      
 
         <div class="row border-top space-after">
           <h1 class="cat-butt">news</h1>
         </div>
         
+        <div class="row">
           <!--Query the database and show my 4 posts from the news category-->
           <?php $custom_query = new
-          WP_Query('posts_per_page=4&category_name=music');
+          WP_Query('posts_per_page=4&category_name=news');
           while($custom_query->have_posts()) : $custom_query->the_post(); ?>
           <!--before you start showing me a post, wrap it in a div with a bootstrap class-->
             <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 push" <?php post_class(); ?> id="post-<?php the_ID(); ?>">
@@ -73,12 +66,14 @@ if( function_exists('fa_display_slider') ){
         
           <?php endwhile; ?>
           <?php wp_reset_postdata(); // reset the query ?>
-        
-                <div class="row border-top space-after">
-          <h1 class="cat-butt">news</h1>
+        </div>
+          
+        <div class="row border-top space-after">
+          <h1 class="cat-butt">music</h1>
         </div>
         
-          <!--Query the database and show my 4 posts from the news category-->
+        <div class="row">
+          <!--Query the database and show my 4 posts from the music category-->
           <?php $custom_query = new
           WP_Query('posts_per_page=4&category_name=music');
           while($custom_query->have_posts()) : $custom_query->the_post(); ?>
@@ -98,14 +93,16 @@ if( function_exists('fa_display_slider') ){
         
           <?php endwhile; ?>
           <?php wp_reset_postdata(); // reset the query ?>
-        
-                <div class="row border-top space-after">
-          <h1 class="cat-butt">news</h1>
+        </div>
+
+        <div class="row border-top space-after">
+          <h1 class="cat-butt">arts</h1>
         </div>
         
-          <!--Query the database and show my 4 posts from the news category-->
+        <div class="row">
+          <!--Query the database and show my 4 posts from the arts category-->
           <?php $custom_query = new
-          WP_Query('posts_per_page=4&category_name=music');
+          WP_Query('posts_per_page=4&category_name=arts');
           while($custom_query->have_posts()) : $custom_query->the_post(); ?>
           <!--before you start showing me a post, wrap it in a div with a bootstrap class-->
             <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 push" <?php post_class(); ?> id="post-<?php the_ID(); ?>">
@@ -123,8 +120,35 @@ if( function_exists('fa_display_slider') ){
         
           <?php endwhile; ?>
           <?php wp_reset_postdata(); // reset the query ?>
+        </div>
         
-     
+          <div class="row border-top space-after">
+          <h1 class="cat-butt">events</h1>
+        </div>
+        
+        <div class="row">
+          <!--Query the database and show my 4 posts from the events category-->
+          <?php $custom_query = new
+          WP_Query('posts_per_page=4&category_name=events');
+          while($custom_query->have_posts()) : $custom_query->the_post(); ?>
+          <!--before you start showing me a post, wrap it in a div with a bootstrap class-->
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 push" <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+            <!--get the thumbnail; you created this with 'featured image'-->
+            <div class="crop-sub"><?php the_post_thumbnail( 'full' ); ?></div>
+            <!--get the post title, wrap it in an h3 tag and make it a hyper link to the actual post-->
+            <h3 class="the-title-sub"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <!--get the author's name-->
+            <p class="author">by&nbsp;<?php the_field('article_author'); ?></p>
+            <!--get the excerpt-->
+            <p class="article_subhead"><?php the_field('article_subhead'); ?></p>
+            <!--get the entire post instead of the excerpt, uncomment below-->
+            <?php /*?><?php the_content(); ?><?php */?>
+            </div>
+        
+          <?php endwhile; ?>
+          <?php wp_reset_postdata(); // reset the query ?>
+        </div>
+  
           
 		</main><!-- #main -->
 	</div><!-- #primary -->
